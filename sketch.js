a59299472034c0f1f9a4dc4f8bcb9f5aff1cef66
PrathameshDev1089 <86548484+PrathameshDev1089@users.noreply.github.com>
2021-09-17 10:30:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple, appleImg;
+var leaf, leafImg;
+var red, redImg
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  leafImg = loadImage("leaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  
+  edges = createEdgeSprites();
+  rabbit.collide(edges);
+
+  rabbit.x=World.mouseX
+
+  var select_sprites = Math.round(random(1,3));
+  
+  if (frameCount % 60 == 0) {
+  if (select_sprites  == 1) 
+  { createApples(); }
+  else if  (select_sprites == 2){
+    createApples(); 
+  }
+
+  else {  createApples(); } }
+  drawSprites();
+  
+  var select_sprites = Math.round(random(1,3));
+
+  if (frameCount % 60 == 0) {
+  if (select_sprites  == 1) 
+  { createLeaves(); }
+  else if  (select_sprites == 5) {
+    createLeaves(); }
+
+  else {  createLeaves(); } }
+  drawSprites();
+}
+
+var select_sprites = Math.round(random(1,3));
+
+if (frameCount % 60 == 0) {
+if (select_sprites  == 1) 
+{ createRedImg(); }
+else if  (select_sprites == 4) {
+  createRedImg(); }
+
+else {  createRedImg(); }
+
+drawSprites();
+}
+
+function createApples() {
+apple=createSprite(random(50,350),40,10,10)
+apple.addImage(appleImg);
+apple.scale=0.05;
+apple.velocityY=5;
+apple.lifetime=130;
+}
+
+function createLeaves() {
+leaf=createSprite(random(50,350),40,10,10)
+leaf.addImage(leafImg);
+leaf.scale=0.07;
+leaf.velocityY=5;
+leaf.lifetime=150;
+}
+
+function createRedImg() {
+  red=createSprite(random(70,350),40,10,10)
+  red.addImage(redImg);
+  red.scale=0.09;
+  red.velocityY=5;
+  red.lifetime=130;
+  }
